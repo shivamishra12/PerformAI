538516b4a5870f0d782bf0b8d45fd7593ee9c9b5
Shivam Mishra <Shivamwork321@gmail.com>
2026-05-18 11:21:13 +0530

    Fix CORS for production deployment

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
